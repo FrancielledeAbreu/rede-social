b22d7541a3b51008bcebc4db23b76f5ede08d31e
Francielle de Abreu <francielledeabreus@gmail.com>
2021-04-18 11:33:05 -0300

    fix: update cache notifications

--- notification/views.py
+++ notification/views.py
@@ -40,3 +40,20 @@ class NotificationView(GenericViewSet, ListModelMixin, UpdateModelMixin, Destroy
         serializer = self.get_serializer(queryset, many=True)
         notification_cache.set_notification(serializer.data)
         return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        notification_cache = NotificationCache(request.user)
+        notification_cache.clear()
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        notification_cache.set_notification(MessageSerializer(
+            Notification.objects.all().filter(user=request.user).filter(read=False), many=True).data)
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+        return Response(serializer.data)
